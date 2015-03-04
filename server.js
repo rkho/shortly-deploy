25ac91b4154b26cf4ffc5500d2ff932a52f63bcd
Hack Reactor Students <accounts@hackreactor.com>
2015-03-04 15:39:52 -0800

    Handling bugs

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ var app = require('./server-config.js');
 
 var port = process.env.SERVER_PORT || 4568;
 
-app.listen(port);
+app.listen(port, function(err, success){
+  if(err){
+    console.log('there was an error');
+  } else {
+    console.log('there was a success!');
+  }
+});
 
 console.log(process);
 console.log('Server now listening on port ' + port);
